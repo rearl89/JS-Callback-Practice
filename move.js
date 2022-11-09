a9e1b8049ef3b664342e3f7dccda6da8ff518ee3
Roger Earl <rogerearl@me.com>
2022-11-08 18:40:58 -0800

    added character direction movement animation

--- move.js
+++ move.js
@@ -6,7 +6,7 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
     
-    function moveWithArrowKeys(left, bottom) {
+    function moveWithArrowKeys(left, bottom, onDirectionChange) {
         let direction = null;
         let x = left;
         let y = bottom;
@@ -48,10 +48,12 @@ function move(element) {
             if(e.key === 'ArrowDown'){
                 direction = 'south'
             }
+            onDirectionChange(direction)
         })
 
         document.addEventListener('keyup', function(e){
             direction = null
+            onDirectionChange(direction)
         })
     }
 
